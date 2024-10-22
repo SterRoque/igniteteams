e123919bd54e303e34459fa51f87357d0ac9c8ec
Sterphane Roque <stermiss2@gmail.com>
2024-10-22 12:39:23 -0300

    Estrutura de navegação

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import {
 } from "@expo-google-fonts/roboto";
 import { StatusBar } from "react-native";
 import { Loading } from "@components/Loading";
+import { Routes } from "@routes/index";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,7 +23,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
