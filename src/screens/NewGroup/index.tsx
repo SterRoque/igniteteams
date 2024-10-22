cf0179f14b83438cb9b96a642036b3229b743a68
Sterphane Roque <stermiss2@gmail.com>
2024-10-22 10:08:49 -0300

    Criando Componente Input

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Header } from "@components/Header";
 import * as S from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
   return (
@@ -14,7 +15,8 @@ export function NewGroup() {
           title="Nova turma"
           subtitle="crie a turma para adicionar as pessoas"
         />
-        <Button title="Criar" />
+        <Input />
+        <Button title="Criar" style={{ marginTop: 20 }} />
       </S.Content>
     </S.Container>
   );
