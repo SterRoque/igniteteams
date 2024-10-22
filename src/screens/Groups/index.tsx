4deb9d414901db25cefc90171560977a0bf3cdd6
Sterphane Roque <stermiss2@gmail.com>
2024-10-22 08:47:22 -0300

    Criando Componente Highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from "@components/Header";
 import * as S from "./styles";
+import { Highlight } from "@components/Highlight";
 
 export function Groups() {
   return (
     <S.Container>
       <Header />
+      <Highlight title="Turmas" subtitle="jogue com sua turma" />
     </S.Container>
   );
 }
