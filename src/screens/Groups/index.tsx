43742101e39d15db25da4476f407e9fd77b8b481
Sterphane Roque <stermiss2@gmail.com>
2024-10-22 09:13:46 -0300

    Listando grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,14 +2,22 @@ import { Header } from "@components/Header";
 import * as S from "./styles";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
+
   return (
     <S.Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com sua turma" />
 
-      <GroupCard title="Galera do ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </S.Container>
   );
 }
