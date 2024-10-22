8284815226a3c827ef4cf45587c12642e51997bd
Sterphane Roque <stermiss2@gmail.com>
2024-10-22 09:22:19 -0300

    Componente List Empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,9 +4,10 @@ import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { useState } from "react";
 import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <S.Container>
@@ -17,6 +18,10 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
     </S.Container>
   );
