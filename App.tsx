7a1caf3da2702d3adf8b968487327650c0429b79
Sterphane Roque <stermiss2@gmail.com>
2024-10-22 11:54:25 -0300

    Recap

--- App.tsx
+++ App.tsx
@@ -1,15 +1,13 @@
 import { Groups } from "@screens/Groups";
 import theme from "@theme/index";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components/native";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
-import { ActivityIndicator, StatusBar } from "react-native";
+import { StatusBar } from "react-native";
 import { Loading } from "@components/Loading";
-import { NewGroup } from "@screens/NewGroup";
-import { Players } from "@screens/Players";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -24,7 +22,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Players /> : <Loading />}
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
