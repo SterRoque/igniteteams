e287565a3e40008b70d9de0de7b84fd305e82f73
Sterphane Roque <stermiss2@gmail.com>
2024-10-22 08:35:17 -0300

    Biblioteca de ícones

--- src/components/Loading/Header/index.tsx
+++ src/components/Loading/Header/index.tsx
@@ -1,9 +1,19 @@
 import * as S from "./styles";
 import logoImg from "@assets/logo.png";
 
-export function Header() {
+type THeader = {
+  showBackButton: boolean;
+};
+
+export function Header({ showBackButton = false }: THeader) {
   return (
     <S.Container>
+      {showBackButton && (
+        <S.BackButton>
+          <S.BackIcon />
+        </S.BackButton>
+      )}
+
       <S.Logo source={logoImg} />
     </S.Container>
   );
