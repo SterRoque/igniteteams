a687af5e17b3cf2a013b44e06c6040dac244e4df
Sterphane Roque <stermiss2@gmail.com>
2024-10-22 12:46:17 -0300

    Tipando as rotas

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      groups: undefined;
+      new: underfined;
+      players: {
+        group: string;
+      };
+    }
+  }
+}
